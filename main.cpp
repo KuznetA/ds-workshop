a8306c113f2068a3221d704200c07be8ff7c7dfd
KuznetA <tolikkuznetsov66@gmail.com>
2024-10-04 14:28:57 -0400

    Print arguments
    It prints the argument passed into the command line when run

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+
+
+int main(int argc, char *argv[]){
+    
+    for(int i = 1; i < argc; i++)
+        std::cout << argv[i] << std::endl;
+}
